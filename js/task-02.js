a8451763f270c2b39a91604c57ed1bf0cc765e72
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-04-18 22:19:19 +0300

    update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.getElementById("ingredients");
+console.log(list);
+for (let i = 0; i < ingredients.length; i++) {
+  const item = document.createElement("li");
+  item.textContent = ingredients[i];
+  item.classList.add("item");
+  list.appendChild(item);
+  console.log(item);
+}
