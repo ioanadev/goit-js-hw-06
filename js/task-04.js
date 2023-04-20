d88693567fa230a604d1e163cf055257a4620b8c
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-04-20 21:17:29 +0300

    update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,29 @@
+let counterValue = document.querySelector("#counter");
+let value = document.querySelector("#value");
+counterValue = 0;
+//console.log(counterValue);
+
+//console.log(value);
+const decrementBtn = document.querySelector(
+  '#counter button[data-action="decrement"]'
+);
+console.log(decrementBtn.dataset.action);
+const handleClickDec = event => {
+  console.log(event);
+  counterValue -= 1;
+  // console.log(counterValue);
+  value.textContent = counterValue;
+};
+const incrementBtn = document.querySelector(
+  '#counter button[data-action="increment"]'
+);
+console.log(incrementBtn.dataset.action);
+const handleClickInc = event => {
+  console.log(event);
+  counterValue += 1;
+  // console.log(counterValue);
+  value.textContent = counterValue;
+};
+
+decrementBtn.addEventListener("click", handleClickDec);
+incrementBtn.addEventListener("click", handleClickInc);
