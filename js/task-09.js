9dcb897c4d70ebbd9ab923fa7135f49fb04e4831
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-04-23 11:45:33 +0300

    update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor);
+const color = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+button.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
